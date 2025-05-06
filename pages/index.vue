a98f3d4caae45de6d974e77f8c7a09a924b1fb6c
kalkadam404 <kalkadamov14@gmail.com>
2025-05-07 00:13:34 +0500

    mini changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Welcome to My Nuxt 3 App</h1>
+    <p>This is a simple example of a Nuxt 3 application.</p>
+    <nuxt-page />
+    <nuxt-link to="/about">Go to About Page</nuxt-link>
+    <nuxt-link to="/contact">Go to Contact Page</nuxt-link>
+  </div>
 </template>
